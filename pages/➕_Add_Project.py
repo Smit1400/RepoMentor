6178b35ac1f16b18e4d97cc82085aed00a10a5f6
smit1400 <shahsmit01042000@gmail.com>
2025-06-25 20:20:22 -0500

    loading github files - indexing and sotring in vectorDB

--- pages/➕_Add_Project.py
+++ pages/➕_Add_Project.py
@@ -1,38 +1,30 @@
+import asyncio
 import streamlit as st
 import datetime
+import os
 
 from src.reminderAI.database.objects import Project
 from src.reminderAI.database.utils import store_object_in_collection
+from src.reminderAI.langchain.indexing import load_github
 
 
 def on_form_submit():
+    load_github(st.session_state.project_git_repo, st.session_state.project_git_branch)
+
     project_object = Project(
         project_name=st.session_state.project_name,
         project_description=st.session_state.project_description,
-        project_git=st.session_state.project_git,
-        project_end_date=st.session_state.project_end_date.isoformat()
+        project_git_repo=st.session_state.project_git_repo,
+        project_git_branch=st.session_state.project_git_branch,
+        project_end_date=st.session_state.project_end_date.isoformat(),
     )
-    result = store_object_in_collection(project_object)
-    if result:
-        st.write("Saved Successfully!")
-
+    if store_object_in_collection(project_object):
+        st.success("Saved Successfully!")
 
 with st.form("add_project", clear_on_submit=True):
-    st.write("Fill Details about the project.")
-    st.text_input("Project Name", placeholder="Portfolio?", key="project_name")
-    st.text_area(
-        "Project Description",
-        placeholder="Summarize whats the projects about and what will the end product look like.",
-        key="project_description",
-    )
-    st.text_input(
-        "GitHub Link", placeholder="Project github link to track", key="project_git"
-    )
-    st.date_input(
-        "Enter desired project end date", datetime.date.today(), key="project_end_date"
-    )
-
-    submitted = st.form_submit_button(
-        "Submit", use_container_width=True, type="primary", on_click=on_form_submit
-    )
-
+    st.text_input("Project Name", key="project_name")
+    st.text_area("Project Description", key="project_description")
+    st.text_input("GitHub Repository Name (username/repo_name)", key="project_git_repo")
+    st.text_input("GitHub Branch to Clone", key="project_git_branch")
+    st.date_input("Desired project end date", datetime.date.today(), key="project_end_date")
+    st.form_submit_button("Submit", on_click=on_form_submit)
